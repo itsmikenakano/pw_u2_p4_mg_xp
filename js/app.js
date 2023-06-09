63f77dac501e00446170e62a6b205d5dad7862a5
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:43:52 -0500

    Taller 14: Navegacion SPA

--- js/app.js
+++ js/app.js
@@ -2,10 +2,29 @@ console.log('Componente Vue')
 console.log(Vue)
 
 const app = Vue.createApp({
-  template:`
-  <h1>Hola Mundo<h1>
-  <p>Desde Vue.js<p>
-  `
+  // template:`
+  // <h1>Hola Mundo</h1>
+  // <p>Desde Vue.js</p>
+  // <p>{{1+1}}</p>
+  // `
+  data(){
+    return{
+      nombre: 'Michael',
+      apellido: 'Garcia',
+      mensaje: 'Hola mundo 2 desde Vue.JS'
+    }
+  },
+
+  methods: {
+      cambiarNombre(){
+        console.log('cambiar nombre')
+        this.nombre = 'Xavier'
+      },
+      cambiarApellido(){
+        console.log('cambiar apellido')
+        this.apellido = 'Paez'
+      }
+  }
 })
 
 //se crea un identificador entre comillas con un numeral obligatorio y un nombre cualquiera
---
+++ js/vFor.js
@@ -0,0 +1,6 @@
+const app = Vue.createApp({
+
+})
+
+//se crea un identificador entre comillas con un numeral obligatorio y un nombre cualquiera
+app.mount('#myApp2')
